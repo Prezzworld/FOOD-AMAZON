be14d2aa1ee8657c7f35a76ffcb4fb8dfa913dc6
Prezzworld <japhetprecious14@gmail.com>
2026-09-25 22:26:02 +0100

    refactor: enhance cart store for guest cart operations with persistence and improved item management

--- FRONTEND/src/store/cartStore.js
+++ FRONTEND/src/store/cartStore.js
@@ -1,39 +1,72 @@
 import { create } from "zustand";
-import { devtools } from "zustand/middleware";
-import { cartService } from "../LANDING-PAGES/utils/cartService";
-import { computeTotals, normalizeCart } from "../LANDING-PAGES/utils/cartUtils";
+import { devtools, persist, createJSONStorage } from "zustand/middleware";
+
+const generateCartId = () => {
+  const timeStamp = Date.now().toString().slice(-6);
+  const random = Math.floor(Math.random() * 1000)
+    .toString()
+    .padStart(3, "0");
+  return `FA${timeStamp}${random}`;
+};
 
 const useCartStore = create(
   devtools(
-    (set, get) => {
+    persist((set, get) => {
       return {
         items: [],
         totalItems: 0,
         totalAmount: 0,
-        loading: false,
-        error: null,
 
-        fetchCart: async () => {
-          try {
-            set({ loading: true });
-            const cartItems = await cartService.getCart();
-            const normalizedCart = normalizeCart(cartItems);
-            const totals = computeTotals(normalizedCart);
-            set({
-              items: [...normalizedCart],
-              totalItems: totals.totalItems,
-              totalAmount: totals.totalAmount,
-            });
-          } catch (error) {
-            set({
-              error: `An error occured, unable to fetch cart ${error.message}`,
-            });
-          } finally {
-            set({ loading: false });
+        addToCart: (product, quantity, variety) => {
+          const existingItemIndex = get().items.findIndex(
+            (item) => item._id === product._id && item.variety === variety,
+          );
+          if (existingItemIndex !== -1) {
+            const updatedItems = get().items.map((item) =>
+              item._id === product._id && item.variety === variety
+                ? {
+                    ...item,
+                    quantity: Math.min(
+                      item.quantity + quantity,
+                      product.inStock,
+                    ),
+                  }
+                : item,
+            );
+            set({ items: updatedItems });
+          } else {
+            const newId = generateCartId();
+            const newItem = {
+              itemId: newId,
+              cartItemId: newId,
+              _id: product._id,
+              name: product.name,
+              price: product.price,
+              productImg: product.productImg,
+              quantity: Math.min(quantity, product.inStock),
+              variety,
+            };
+            const newItems = [...get().items, newItem];
+            set({ items: newItems });
           }
         },
+        updateItem: (itemId, quantity) => {
+          const updatedItems = get().items.map((item) =>
+            item.itemId === itemId ? { ...item, quantity: quantity } : item,
+          );
+          set({ items: updatedItems });
+        },
+        removeItem: (itemId) => {
+          const itemAfterRemove = get().items.filter(
+            (item) => item.itemId !== itemId,
+          );
+          set({ items: itemAfterRemove });
+        },
+        clearCart: () => {
+          set({ items: [], totalItems: 0, totalAmount: 0 });
+        },
       };
-    },
+    }, {name: "guest-cart-storage", storage: createJSONStorage(() => localStorage), partialize: (state) => ({items: state.items})}),
     { name: "CartStore", enabled: true },
   ),
 );
@@ -49,4 +82,4 @@ const useCartStore = create(
 // };
 // runTest();
 
-export default useCartStore
+export default useCartStore;
